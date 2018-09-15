1a581178ff8e75c37498b00880ce5ebed1e86c26
Seth Nabarro <s.nabarro@hotmail.com>
2018-09-15 13:03:32 +0100

    updated load_data functions

--- load_data.py
+++ load_data.py
@@ -8,43 +8,25 @@ _TEST_DATA_PATH = 'data/SemEval2016-Task6-subtaskA-testdata-gold.txt'
 
 def load_train_data(csv_path=_TRAIN_DATA_PATH):
     """
-    Read in training data. Adapted from 'stance detection data processing.html'
+    Read in training data.
     :param csv_path: filepath to training csv
     :type csv_path: str
     :return training data table
     :type pandas DataFrame
     """
-    data = []
-
-    with open(csv_path, 'r',
-              encoding="iso-8859-1") as fin:
-        reader = csv.reader(fin, quotechar='"')
-        columns = next(reader)
-        for line in reader:
-            data.append(line)
-
-    train_df = pd.DataFrame(data, columns=columns)
+    train_df = pd.read_csv(open(csv_path, 'r', encoding="iso-8859-1"), quotechar='"')
 
     return train_df
 
 
 def load_test_data(txt_path=_TEST_DATA_PATH):
     """
-    Read in test data. Adapted from 'stance detection data processing.html'
+    Read in test data
     :param txt_path: filepath to test txt file
     :type txt_path: str
     :return test data table
     :type pandas DataFrame
     """
-    data = []
-
-    with open(txt_path, 'r',
-              encoding="iso-8859-1") as fin:
-        reader = csv.reader(fin, delimiter='\t')
-        columns = next(reader)
-        for line in reader:
-            data.append(line)
-
-    test_df = pd.DataFrame(data, columns=columns)
+    test_df = pd.read_csv(open(txt_path, 'r', encoding="iso-8859-1"), delimiter='\t')
 
     return test_df
